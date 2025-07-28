0c66689d026369a7f90d98880b5c78e2eb740261
3nkhtul9a <153745174+3nkhtul9a@users.noreply.github.com>
2025-07-28 19:34:15 +0200

    Update task2.js

--- task2.js
+++ task2.js
@@ -6,12 +6,12 @@ function parallaxScroll(){
    var scrolled = $(window).scrollTop(); 
    $('.logo').css({
      'top': (0-(scrolled*0.5)) + 'px',
-     'transform': 'scale(' + Math.max(1 - scrolled * 0.01, 0.1) + ')' 
+     'transform': 'scale(' + Math.max(0.5, Math.min(1 - scrolled * 0.01, 1)) + ')'
    });
    $('.x-wing').css({
       top: (5 + scrolled * 0.5) + 'px',
       left: (scrolled * 1.5) + 'px'
     });
-   $('.deathstar').css('left',(1200-(scrolled*.4))+'px');
+   $('.stardestroyer').css('left',(1200-(scrolled*.4))+'px');
    $('.text').css('top',(200-(scrolled*.5))+'px'); 
 }
